a28366101b6b1adc530e4efe00b979b9f054c27d
David Deaton <dideater@gmail.com>
2020-07-03 22:04:37 -0700

    Quiz Timer complete

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-var x = 20;
-var y = "string";
-var z = x + y;
+var startQuiz = document.getElementById("start");
+var display = document.getElementById("display");
+var timer = 10;
+var preTimer = 5.5;
+var disappear = document.getElementsByClassName("disappear");
 
-console.log(z);
+startQuiz.addEventListener("click", function() {
+
+    var removeBtn = document.getElementById("removeBtn");
+    removeBtn.parentNode.removeChild(removeBtn);
+
+    var footTimer = document.getElementById("footer");
+    footTimer.textContent = "Timer: " + timer;
+
+
+    var preTimerInterval = setInterval(function() {
+    preTimer--;
+
+    if(preTimer >= 4) {
+       display.textContent = "Start!";
+    }
+
+    else if (preTimer < 4) {
+        display.textContent = "";
+        timer--;
+        footTimer.textContent = "Timer: " + timer;
+
+        if (timer <= 0) {
+            display.textContent = "Failure!"
+        }
+    }
+      
+
+       
+    
+    },1000);
+   
+
+   
+  
+});
